1ba7656a9036121c557bb63608bfe5f43fdcb998
aliki212 <aliki212@gmail.com>
2020-02-10 20:44:40 +0200

    Created Details and INdex and ViewerController to make a view of all viewers and then click on them to see their details(So far their name)

---
+++ Controllers/ViewerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootFlixBC9.Models;
+
+namespace BootFlixBC9.Controllers
+{
+    public class ViewerController : Controller
+    {
+        private IEnumerable<Viewer> GetViewers()
+        {
+            return new List<Viewer>
+            {
+                new Viewer { Id = 1 , Name = "Costas Spyrou"},
+                new Viewer { Id = 2 , Name = "Pavlina Kostalea"}
+            };
+        }
+        // GET: Viewer
+        public ActionResult Index()
+        {
+            var viewers = GetViewers();
+            return View(viewers);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var viewer = GetViewers().SingleOrDefault(v => v.Id == id);
+            if (viewer == null)
+                return HttpNotFound();
+
+            return View(viewer);
+        }
+    }
+}
