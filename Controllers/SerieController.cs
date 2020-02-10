c4907552172818ce90167f2755d4be028a759c09
aliki212 <aliki212@gmail.com>
2020-02-10 18:26:37 +0200

    Add project files.

---
+++ Controllers/SerieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootFlixBC9.Controllers
+{
+    public class SerieController : Controller
+    {
+        // GET: Serie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Serie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootFlixBC9.Models
+{
+    public class Serie
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
