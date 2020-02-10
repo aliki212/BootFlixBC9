c96393912b973d1c5b1e36d694b890823a20c70c
aliki212 <aliki212@gmail.com>
2020-02-10 18:53:33 +0200

    Made the SerieController > index ->Perfect and view of Perfect.cshtml Then Added Cyborg.css

--- Controllers/SerieController.cs
+++ Controllers/SerieController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BootFlixBC9.Models;
 
 namespace BootFlixBC9.Controllers
 {
     public class SerieController : Controller
     {
-        // GET: Serie
-        public ActionResult Index()
+        // GET: Serie/Perfect
+        public ActionResult Perfect()
         {
-            return View();
+            var serie = new Serie()
+            {
+                Name = "The Wire"
+            };
+            return View(serie);
         }
     }
 }
