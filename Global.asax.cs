f7fe7bf1e38c0285661768d7ac814ab6bee22703
aliki212 <aliki212@gmail.com>
2020-02-17 18:55:40 +0200

    Added ViewerSController > difference in the "s" as it is an API Controller vs a Model controller - same name would work but confusing Made the changes of the poppedup readme file in Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Routing;
 
 namespace BootFlixBC9
 {
@@ -12,6 +14,7 @@ namespace BootFlixBC9
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
