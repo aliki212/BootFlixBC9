0278122fef7f9ee12a7c5ebfb660b8ae993b43c0
aliki212 <aliki212@gmail.com>
2020-02-17 21:32:00 +0200

    Created API for Serie: 1) Create SeriesController 2) Create SerieDto Class with same properties as Serie & no relationship properties

---
+++ Controllers/API/SeriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BootFlixBC9.Models;
+using AutoMapper;
+using BootFlixBC9.DTOs;
+
+
+namespace BootFlixBC9.Controllers.API
+{
+    public class SeriesController : ApiController
+    {
+
+        private ApplicationDbContext context;
+        //part2
+        public SeriesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+    }
+}
---
+++ DTOs/SerieDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BootFlixBC9.DTOs
+{
+    public class SerieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(225)]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Date Released")]
+
+        public DateTime DateReleased { get; set; }
+
+        public byte Seasons { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        //public Genre Genre { get; set; }
+    }
+}
