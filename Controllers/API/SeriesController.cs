887a897b6731eda206837fa71d1b8051bcd7d661
aliki212 <aliki212@gmail.com>
2020-02-17 21:41:27 +0200

    4) apply all methods from ViewersController to SeriesController

--- Controllers/API/SeriesController.cs
+++ Controllers/API/SeriesController.cs
@@ -21,5 +21,69 @@ namespace BootFlixBC9.Controllers.API
             context = new ApplicationDbContext();
         }
 
+
+        //GET /api/series
+
+        public IEnumerable<SerieDto> GetSeries()
+        {
+            return context.Series.ToList()
+                .Select(Mapper.Map<Serie, SerieDto>);
+        }
+
+        // GET /api/series/id
+        public SerieDto GetSeries(int id)
+        {
+            var serie = context.Series.SingleOrDefault(c => c.Id == id);
+            if (serie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Mapper.Map<Serie, SerieDto>(serie);
+        }
+
+        // POST /api/viewers
+        [HttpPost]
+        public SerieDto CreateSerie(SerieDto serieDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            var serie = Mapper.Map<SerieDto, Serie>(serieDto);
+
+            context.Series.Add(serie);
+            context.SaveChanges();
+            return serieDto;
+        }
+
+        // PUT /api/serie/id
+
+        [HttpPut]
+        public void UpdateSerie(int id, SerieDto serieDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var serieDb = context.Viewers.SingleOrDefault(c => c.Id == id);
+            if (serieDb == null) // if the db viewer from the id =null
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(serieDto, serieDb);
+            //viewerDb.Name = viewer.Name;
+            //viewerDb.Birtdate = viewer.Birtdate;
+            //viewerDb.IsSubscribedToNews = viewer.IsSubscribedToNews;
+            //viewerDb.MembershipTypeId = viewer.MembershipTypeId;
+
+            context.SaveChanges();
+        }
+
+        //DELETE /api/viewer/id
+        //no changes for Dto implementation
+        [HttpDelete]
+        public void DeleteSerie(int id)
+        {
+            var serieDb = context.Series.SingleOrDefault(v => v.Id == id);
+            if (serieDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            context.Series.Remove(serieDb);
+            context.SaveChanges();
+        }
     }
 }
