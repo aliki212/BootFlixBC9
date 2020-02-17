2d3e105a55fcb86568e746bb9ede46fa2d5c7de1
aliki212 <aliki212@gmail.com>
2020-02-17 19:56:07 +0200

    Created GET method and then Post, then Post with the same GET link via postman(installed app)

--- Controllers/API/ViewersController.cs
+++ Controllers/API/ViewersController.cs
@@ -1,13 +1,50 @@
-﻿using System;
+﻿using BootFlixBC9.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity;
 
 namespace BootFlixBC9.Controllers.API
 {
     public class ViewersController : ApiController
     {
-    }
+        private ApplicationDbContext context = new ApplicationDbContext();
+        //public class ViewersController 
+        //{ 
+        //    context = new ApplicationDbContext;
+        //}
+
+
+            //GET /api/viewers
+        public IEnumerable<Viewer> GetViewers()
+        {
+            return context.Viewers.ToList();
+        }
+
+        // GET /api/viewers/id
+        public Viewer GetViewer(int id)
+        {
+            var viewer = context.Viewers.SingleOrDefault(c => c.Id == id);
+                if(viewer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return viewer;
+        }
+
+        // POST /api/viewers
+        [HttpPost]
+        public Viewer CreateViewer(Viewer viewer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            context.Viewers.Add(viewer);
+            context.SaveChanges();
+            return viewer;
+        }
+
+
+
+    }//
 }
