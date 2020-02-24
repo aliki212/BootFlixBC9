91b2c832301b09b84d981e4a41be48a81b266e1d
aliki212 <aliki212@gmail.com>
2020-02-24 20:32:00 +0200

    Created the Contract Simple MVC controller (not API) that creates a folder in the Views, then a New view and edited the NavBar/partial to add a link to New Contract

---
+++ Controllers/ContractController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootFlixBC9.Controllers
+{
+    public class ContractController : Controller
+    {
+        // GET: Contract
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
