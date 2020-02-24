6c99ddd24e0798e43ea8598b2240e886e7b55ecb
aliki212 <aliki212@gmail.com>
2020-02-24 19:07:20 +0200

    Created ActorDto and NewContractDto > info in comments

---
+++ DTOs/ActorDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BootFlixBC9.DTOs
+{
+    public class ActorDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Nationality { get; set; }
+
+        //public bool IsRegistered { get; set; }
+    }
+}
---
+++ DTOs/NewContractDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootFlixBC9.DTOs
+{
+    public class NewContractDto
+    {
+        public int ActorId { get; set; }
+
+        public List<int> SerieIds { get; set; } 
+        // Same as having one Serie and a list of Actors for a MANY to MANY relationship
+        // Here we had the Series ready so it will be easier for customers to add the existing series in the new Actor Contracts
+    }
+}
